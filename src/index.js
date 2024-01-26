762228a9632b6b00c65982d27ad3bf272752fbb1
pouria pou <pouriap024@yahoo.com>
2024-01-26 13:01:02 +0100

    implemented obstacle and obstacle movement

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ class Player {
     this.positionY = 50;
     this.width = 20;
     this.height = 20;
-    this.domElement = undefined;
+    this.domElement;
     this.createDomElem(); //invoke to create the element
   }
   createDomElem() {
@@ -30,7 +30,39 @@ class Player {
   }
 }
 
+class Obstacle {
+  constructor() {
+    this.positionX = 50;
+    this.positionY = 100;
+    this.width = 20;
+    this.height = 20;
+
+    this.domElement;
+    this.createObstacleElem();
+  }
+  createObstacleElem() {
+    this.domElement = document.createElement("div");
+    this.domElement.setAttribute("class", "obstacle");
+    const boardElement = document.querySelector("#board");
+    boardElement.appendChild(this.domElement);
+    //add css styling with DOM
+    this.domElement.style.width = this.width + "vw";
+    this.domElement.style.height = this.height + "vh";
+    //add position styling with DOM
+    this.domElement.style.bottom = this.positionY + "vw";
+    this.domElement.style.left = this.positionX + "vh";
+  }
+  moveDown() {
+    this.positionY -= 10;
+    this.domElement.style.bottom = this.positionY + "vh";
+  }
+}
+
 const player = new Player();
+const obstacle = new Obstacle();
+setInterval(() => {
+  obstacle.moveDown();
+}, 5 * 1000);
 window.addEventListener("keydown", (e) => {
   if (e.code === "ArrowLeft") {
     player.moveLeft();
